8db7d10073e1b47ef59f3b0e6379285d6dbf48c9
江芷酱紫 <wanjiangzhi@outlook.com>
2026-02-26 16:02:48 +0800

    Initial commit

---
+++ example/example.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <MemoryToolsCPP.hpp>
+
+int main() {
+    int aalll
+    int val = 0;
+    MemoryTools::Addr val_addr(&val);
+
+    std::cout << static_cast<uintptr_t>(val_addr) << std::endl;
+
+    std::cout << val << std::endl;
+
+    val_addr.writeType<decltype(val)>(999);
+
+    std::cout << val << std::endl;
+
+    return 0;
+}
